e813d9b576846d8932d10a066cd951bccb6fb346
tonghm <tonghm@chinatelecom.cn>
2024-02-02 15:29:25 +0800

    项目新建

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("orc项目启动成功");
+        System.out.println("////////////////////////////////////////////////////////////////////\n" +
+                "//                          _ooOoo_                               //\n" +
+                "//                         o8888888o                              //\n" +
+                "//                         88\" . \"88                              //\n" +
+                "//                         (| ^_^ |)                              //\n" +
+                "//                         O\\  =  /O                              //\n" +
+                "//                      ____/`---'\\____                           //\n" +
+                "//                    .'  \\\\|     |//  `.                         //\n" +
+                "//                   /  \\\\|||  :  |||//  \\                        //\n" +
+                "//                  /  _||||| -:- |||||-  \\                       //\n" +
+                "//                  |   | \\\\\\  -  /// |   |                       //\n" +
+                "//                  | \\_|  ''\\---/''  |   |                       //\n" +
+                "//                  \\  .-\\__  `-`  ___/-. /                       //\n" +
+                "//                ___`. .'  /--.--\\  `. . ___                     //\n" +
+                "//              .\"\" '<  `.___\\_<|>_/___.'  >'\"\".                  //\n" +
+                "//            | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |                 //\n" +
+                "//            \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /                 //\n" +
+                "//      ========`-.____`-.___\\_____/___.-`____.-'========         //\n" +
+                "//                           `=---='                              //\n" +
+                "//      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //\n" +
+                "//            佛祖保佑       永不宕机     永无BUG                    //\n" +
+                "////////////////////////////////////////////////////////////////////");
+    }
 
 }
--- src/main/java/com/example/demo/test/index.java
+++ src/main/java/com/example/demo/test/index.java
@@ -1,6 +1,20 @@
-package com.example.demo.test;/**
+package com.example.demo.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
  * @author wyx
  * &#064;date  2024/2/2 14:53
  * @version 1.0
- */public class index {
+ */
+@Controller
+public class index {
+
+    @RequestMapping("/index")
+    @ResponseBody
+    public String index(){
+        return "hello world!";
+    }
 }
